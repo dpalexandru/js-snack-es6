ffafbea587c5c404856a3c62ac106fb45a48997b
dpalexandru <alexpoian91@hotmail.com>
2025-07-14 15:29:38 +0200

    feat: create an array of objects

---
+++ assets/js/script.js
@@ -0,0 +1,21 @@
+// Creo un array di di bici
+const bici = [{
+        nome: "bianchi",
+        peso: 34,
+    },
+    {
+        nome: "fiido",
+        peso: 30,
+
+    },
+    {
+        nome: "liido",
+        peso: 18,
+
+    },
+    {
+        nome: "bicic",
+        peso: 10,
+    },
+]
+console.log(bici)
